215fc98d3b400a3664ee7d31161ac92151a47990
Sergey1-git <Sergey1-git.yan.dex.ru@yandex.ru>
2025-08-07 23:58:26 +0300

    add func editing_a_path into files func_cli_files_manager.py, test_func_cli_files_manager.py

--- package_cli_files_manager/test_func_cli_files_manager.py
+++ package_cli_files_manager/test_func_cli_files_manager.py
@@ -6,25 +6,17 @@ class TestPreparing_for_work(unittest.TestCase):
     def test_preparing_for_work(self):
         print('Проверка, что функция preparing_for_work создает папку folder_test.')
         # создание папки folder_test
-        path_folder_test = os.getcwd()
-        print('@@@',path_folder_test)
-        #absolute_path = os.path.abspath('CLI_files_manager/folder_testh')
-        if 'package_cli_files_manager' in path_folder_test:
-            folder_test=path_folder_test.replace('\\package_cli_files_manager','')
-            #folder_test = path_folder_test.replace('', '')
-            print('@@@##',folder_test)
-        else:
-            folder_test=path_folder_test
-        preparing_for_work(folder_test)
-        print('@@@##AAA', folder_test)
-        #self.assertTrue(os.path.exists(folder_test))
-        self.assertIn('folder_test', os.listdir(folder_test))
+        path_folder=editing_a_path()
+        preparing_for_work(path_folder)
+        self.assertIn('folder_test', os.listdir(path_folder))
 
 class TestFind_Folders_And_Files(unittest.TestCase):
     def test_find_folders(self):
         print('Проверка, что функция find_folders_and_files определяет пути папок.')
         folder_name='folder_test'
         file_name=None
+        path_folder = editing_a_path()
+        path_folder_test = os.path.join(path_folder, 'folder_test')
         self.assertEqual(find_folders_and_files(folder_name, file_name), path_folder_test)
 
 
@@ -39,6 +31,9 @@ class TestFind_Folders_And_Files(unittest.TestCase):
         print('Проверка, что функция find_folders_and_files определяет путь файла.')
         file_name = 'test1.txt'
         folder_name='folder_test'
+        path_folder = editing_a_path()
+        path_folder_test = os.path.join(path_folder, 'folder_test')
+        path_test1 = os.path.join(path_folder_test, 'test1.txt')
         self.assertEqual(find_folders_and_files(folder_name,file_name),path_test1)
 
 
@@ -52,7 +47,23 @@ class TestCount_files_recursive(unittest.TestCase):
     def test_count_files_recursive(self):
     # подсчет файлов в папка 'folder_test'
         print('Проверка, что функция count_files_recursive возвращает количество файлов в папке.')
+        path_folder = editing_a_path()
+        path_folder_test = os.path.join(path_folder, 'folder_test')
         total_files = 0
         for root,dirs, files in os.walk(path_folder_test):
             total_files+=len(files)
         self.assertEqual(count_files_recursive(path_folder_test),total_files)
+
+def editing_a_path():
+    path_folder_test = os.getcwd()
+    print('@@@', path_folder_test)
+    # absolute_path = os.path.abspath('CLI_files_manager/folder_testh')
+    if 'package_cli_files_manager' in path_folder_test:
+        folder_test = path_folder_test.replace('\\package_cli_files_manager', '')
+        # folder_test = path_folder_test.replace('', '')
+        print('@@@##', folder_test)
+        return folder_test
+    else:
+        folder_test = path_folder_test
+        print('@@@##', folder_test)
+        return folder_test
